aeeb6273b1e970695294788a5d45d24edf402722
Lucas Silva <lucassilvayoshi@gmail.com>
2024-09-01 11:49:53 -0300

    alterando cv e centralizando services

--- src/components/services/Service.jsx
+++ src/components/services/Service.jsx
@@ -7,25 +7,33 @@ const Service = () => {
       <span className="section__subtitle">O que eu ofereço</span>
 
       <div className="services-container">
+        <div className="flex-box">
         <div className="service-box">
           <div className="service-info">
-            <h4 className="box1">Criação de Sites</h4>
+            <div className="texto-center">
+              <h4 className="box1">Criação de Sites</h4>
             <p>
               Transformo suas ideias em um site profissional e atrativo, focado
               em transmitir sua mensagem de forma clara e eficaz.
             </p>
+            </div>
           </div>
         </div>
+        </div>
 
+        <div className="flex-box">
         <div className="service-box">
           <div className="service-info parte2">
-            <h4 className="box2">Criação de Landing Pages</h4>
+            <div className="texto-center">
+            <h4 className="box2">Criação de <br />Landing Pages</h4>
             <p>
               Crio páginas de destino impactantes e otimizadas para converter
               visitantes em clientes, utilizando técnicas comprovadas de design
               e persuasão.
             </p>
+            </div>
           </div>
+        </div>
         </div>
       </div>
     </section>
